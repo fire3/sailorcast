15bbff5d97611673b3279a57e07b903cd377cfc6
fire3 <wxingyan@gmail.com>
2014-12-26 23:01:43 +0800

    android: update SCSite toString method.

--- android/SailorCast/src/main/java/com/crixmod/sailorcast/model/SCSite.java
+++ android/SailorCast/src/main/java/com/crixmod/sailorcast/model/SCSite.java
@@ -8,10 +8,26 @@ public class SCSite {
     public static int SOHU = 1;
     public static int IQIYI = 2;
     public static int YOUKU = 3;
+    private String mSiteName;
 
     private int mSiteID;
 
     public SCSite(int mSiteID) {
         this.mSiteID = mSiteID;
+        if(mSiteID == SOHU)
+            mSiteName = "Sohu";
+        if(mSiteID == YOUKU)
+            mSiteName = "Youku";
+        if(mSiteID == IQIYI)
+            mSiteName = "IQiyi";
+        if(mSiteID == UNKNOWN)
+            mSiteName = "Unknown";
+    }
+
+    @Override
+    public String toString() {
+        return "SCSite{" +
+                "mSiteID=" + mSiteName +
+                '}';
     }
 }
