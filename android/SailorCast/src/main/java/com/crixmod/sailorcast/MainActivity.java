5ce44d197d22aadd2e8ca6602b5940e8351d0428
fire3 <wxingyan@foxmail.com>
2014-12-26 17:03:06 +0800

    android: rename app to SailorCast.

--- android/SailorCast/src/main/java/com/crixmod/sailorcast/MainActivity.java
+++ android/SailorCast/src/main/java/com/crixmod/sailorcast/MainActivity.java
@@ -34,16 +34,6 @@ public class MainActivity extends SCDrawerActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
