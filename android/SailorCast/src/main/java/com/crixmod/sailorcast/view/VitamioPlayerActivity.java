ecc0e5714936f5e15a362173b2d279e130cb626b
fire3 <wxingyan@foxmail.com>
2015-02-10 10:54:32 +0800

    android: 整理代码。

--- android/SailorCast/src/main/java/com/crixmod/sailorcast/view/VitamioPlayerActivity.java
+++ android/SailorCast/src/main/java/com/crixmod/sailorcast/view/VitamioPlayerActivity.java
@@ -1,26 +1,9 @@
-/*
- * Copyright (C) 2013 yixia.com
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.crixmod.sailorcast.view;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.media.AudioManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -30,7 +13,6 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.crixmod.sailorcast.R;
 
@@ -45,69 +27,63 @@ public class VitamioPlayerActivity extends Activity
         implements OnBufferingUpdateListener,
         MediaPlayer.OnInfoListener,
         OnCompletionListener, OnPreparedListener, OnVideoSizeChangedListener, SurfaceHolder.Callback,
-VideoControllerView.MediaPlayerControl{
+        VideoControllerView.MediaPlayerControl {
 
-	private static final String TAG = "MediaPlayerDemo";
-	private int mVideoWidth;
-	private int mVideoHeight;
-	private MediaPlayer mMediaPlayer;
-	private SurfaceView mPreview;
-	private SurfaceHolder holder;
-	private String path;
-	private Bundle extras;
-	private static final String MEDIA = "media";
-	private static final int LOCAL_AUDIO = 1;
-	private static final int STREAM_AUDIO = 2;
-	private static final int RESOURCES_AUDIO = 3;
-	private static final int LOCAL_VIDEO = 4;
-	private static final int STREAM_VIDEO = 5;
-	private boolean mIsVideoSizeKnown = false;
-	private boolean mIsVideoReadyToBePlayed = false;
+    private static final String TAG = "MediaPlayerDemo";
+    private int mVideoWidth;
+    private int mVideoHeight;
+    private MediaPlayer mMediaPlayer;
+    private SurfaceView mPreview;
+    private SurfaceHolder holder;
+    private String path;
+    private Bundle extras;
+    private static final String MEDIA = "media";
+    private boolean mIsVideoSizeKnown = false;
+    private boolean mIsVideoReadyToBePlayed = false;
     private VideoControllerView controller;
     private boolean needResume;
     private LinearLayout mLoadingView;
     private TextView mLoadingPercentView;
 
     /**
-	 * 
-	 * Called when the activity is first created.
-	 */
-	@Override
-	public void onCreate(Bundle icicle) {
-		super.onCreate(icicle);
-		if (!LibsChecker.checkVitamioLibs(this))
-			return;
-		setContentView(R.layout.activity_vitamio_player);
-		mPreview = (SurfaceView) findViewById(R.id.surface);
-		holder = mPreview.getHolder();
-		holder.addCallback(this);
-		holder.setFormat(PixelFormat.RGBA_8888); 
-		extras = getIntent().getExtras();
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        if (!LibsChecker.checkVitamioLibs(this))
+            return;
+        setContentView(R.layout.activity_vitamio_player);
+        mPreview = (SurfaceView) findViewById(R.id.surface);
+        holder = mPreview.getHolder();
+        holder.addCallback(this);
+        holder.setFormat(PixelFormat.RGBA_8888);
+        extras = getIntent().getExtras();
         mLoadingView = (LinearLayout) findViewById(R.id.loading);
         mLoadingView.setVisibility(View.INVISIBLE);
         mLoadingPercentView = (TextView) findViewById(R.id.loading_percent);
-	}
+    }
 
-	private void playVideo() {
-		doCleanUp();
-		try {
-			// Create a new media player and set the listeners
-			mMediaPlayer = new MediaPlayer(this);
-			mMediaPlayer.setDataSource(path);
-			mMediaPlayer.setDisplay(holder);
-			mMediaPlayer.prepareAsync();
-			mMediaPlayer.setOnBufferingUpdateListener(this);
+    private void playVideo() {
+        doCleanUp();
+        try {
+            // Create a new media player and set the listeners
+            mMediaPlayer = new MediaPlayer(this);
+            mMediaPlayer.setDataSource(path);
+            mMediaPlayer.setDisplay(holder);
+            mMediaPlayer.prepareAsync();
+            mMediaPlayer.setOnBufferingUpdateListener(this);
             mMediaPlayer.setOnInfoListener(this);
-			mMediaPlayer.setOnCompletionListener(this);
-			mMediaPlayer.setOnPreparedListener(this);
-			mMediaPlayer.setOnVideoSizeChangedListener(this);
+            mMediaPlayer.setOnCompletionListener(this);
+            mMediaPlayer.setOnPreparedListener(this);
+            mMediaPlayer.setOnVideoSizeChangedListener(this);
             controller = new VideoControllerView(this);
-			setVolumeControlStream(AudioManager.STREAM_MUSIC);
+            setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
-		} catch (Exception e) {
-			Log.e(TAG, "error: " + e.getMessage(), e);
-		}
-	}
+        } catch (Exception e) {
+            Log.e(TAG, "error: " + e.getMessage(), e);
+        }
+    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -115,113 +91,113 @@ VideoControllerView.MediaPlayerControl{
         return false;
     }
 
-	public void onBufferingUpdate(MediaPlayer arg0, int percent) {
-		// Log.d(TAG, "onBufferingUpdate percent:" + percent);
-        mLoadingPercentView.setText(""+percent+"%");
-	}
+    public void onBufferingUpdate(MediaPlayer arg0, int percent) {
+        // Log.d(TAG, "onBufferingUpdate percent:" + percent);
+        mLoadingPercentView.setText("" + percent + "%");
+    }
 
-	public void onCompletion(MediaPlayer arg0) {
-		Log.d(TAG, "onCompletion called");
-	}
+    public void onCompletion(MediaPlayer arg0) {
+        Log.d(TAG, "onCompletion called");
+    }
 
-	public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
-		Log.v(TAG, "onVideoSizeChanged called");
-		if (width == 0 || height == 0) {
-			Log.e(TAG, "invalid video width(" + width + ") or height(" + height + ")");
-			return;
-		}
-		mIsVideoSizeKnown = true;
-		mVideoWidth = width;
-		mVideoHeight = height;
-		if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
-			startVideoPlayback();
-		}
-	}
+    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+        Log.v(TAG, "onVideoSizeChanged called");
+        if (width == 0 || height == 0) {
+            Log.e(TAG, "invalid video width(" + width + ") or height(" + height + ")");
+            return;
+        }
+        mIsVideoSizeKnown = true;
+        mVideoWidth = width;
+        mVideoHeight = height;
+        if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
+            startVideoPlayback();
+        }
+    }
 
-	public void onPrepared(MediaPlayer mediaplayer) {
-		Log.d(TAG, "onPrepared called");
-		mIsVideoReadyToBePlayed = true;
-		if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
-			startVideoPlayback();
+    public void onPrepared(MediaPlayer mediaplayer) {
+        Log.d(TAG, "onPrepared called");
+        mIsVideoReadyToBePlayed = true;
+        if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
+            startVideoPlayback();
 
             controller.setMediaPlayer(this);
             controller.setAnchorView((FrameLayout) findViewById(R.id.videoSurfaceContainer));
-		}
-	}
+        }
+    }
 
-	public void surfaceChanged(SurfaceHolder surfaceholder, int i, int j, int k) {
-		Log.d(TAG, "surfaceChanged called");
+    public void surfaceChanged(SurfaceHolder surfaceholder, int i, int j, int k) {
+        Log.d(TAG, "surfaceChanged called");
 
-	}
+    }
 
-	public void surfaceDestroyed(SurfaceHolder surfaceholder) {
-		Log.d(TAG, "surfaceDestroyed called");
-	}
+    public void surfaceDestroyed(SurfaceHolder surfaceholder) {
+        Log.d(TAG, "surfaceDestroyed called");
+    }
 
-	public void surfaceCreated(SurfaceHolder holder) {
-		Log.d(TAG, "surfaceCreated called");
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.d(TAG, "surfaceCreated called");
         path = extras.getString(MEDIA);
-		playVideo();
+        playVideo();
 
-	}
+    }
 
     public static void launch(Activity fromActivity, String url) {
         Intent mpdIntent = new Intent(fromActivity, VitamioPlayerActivity.class)
-                .putExtra(MEDIA,url);
+                .putExtra(MEDIA, url);
         fromActivity.startActivity(mpdIntent);
     }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		releaseMediaPlayer();
-		doCleanUp();
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseMediaPlayer();
+        doCleanUp();
+    }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		releaseMediaPlayer();
-		doCleanUp();
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseMediaPlayer();
+        doCleanUp();
+    }
 
-	private void releaseMediaPlayer() {
-		if (mMediaPlayer != null) {
-			mMediaPlayer.release();
-			mMediaPlayer = null;
-		}
-	}
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
 
-	private void doCleanUp() {
-		mVideoWidth = 0;
-		mVideoHeight = 0;
-		mIsVideoReadyToBePlayed = false;
-		mIsVideoSizeKnown = false;
-	}
+    private void doCleanUp() {
+        mVideoWidth = 0;
+        mVideoHeight = 0;
+        mIsVideoReadyToBePlayed = false;
+        mIsVideoSizeKnown = false;
+    }
 
-	private void startVideoPlayback() {
-		Log.v(TAG, "startVideoPlayback");
-		holder.setFixedSize(mVideoWidth, mVideoHeight);
-		mMediaPlayer.start();
-	}
+    private void startVideoPlayback() {
+        Log.v(TAG, "startVideoPlayback");
+        holder.setFixedSize(mVideoWidth, mVideoHeight);
+        mMediaPlayer.start();
+    }
 
     @Override
     public void start() {
-        if(mMediaPlayer != null)
+        if (mMediaPlayer != null)
             mMediaPlayer.start();
 
     }
 
     @Override
     public void pause() {
-        if(mMediaPlayer != null)
+        if (mMediaPlayer != null)
             mMediaPlayer.pause();
 
     }
 
     @Override
     public int getDuration() {
-        if(mMediaPlayer != null)
+        if (mMediaPlayer != null)
             return (int) mMediaPlayer.getDuration();
         else
             return 0;
@@ -229,7 +205,7 @@ VideoControllerView.MediaPlayerControl{
 
     @Override
     public int getCurrentPosition() {
-        if(mMediaPlayer != null)
+        if (mMediaPlayer != null)
             return (int) mMediaPlayer.getCurrentPosition();
         else
             return 0;
@@ -237,14 +213,14 @@ VideoControllerView.MediaPlayerControl{
 
     @Override
     public void seekTo(int pos) {
-        if(mMediaPlayer != null)
+        if (mMediaPlayer != null)
             mMediaPlayer.seekTo(pos);
 
     }
 
     @Override
     public boolean isPlaying() {
-        if(mMediaPlayer != null)
+        if (mMediaPlayer != null)
             return mMediaPlayer.isPlaying();
         else
             return false;
@@ -306,12 +282,12 @@ VideoControllerView.MediaPlayerControl{
     }
 
     private void startPlayer() {
-        if(mMediaPlayer != null)
+        if (mMediaPlayer != null)
             mMediaPlayer.start();
     }
 
     private void stopPlayer() {
-        if(mMediaPlayer != null)
+        if (mMediaPlayer != null)
             mMediaPlayer.stop();
     }
 }
