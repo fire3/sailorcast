214a52d75b8aec95e99c5f51f65d3ae0c2fb4d9d
fire3 <wxingyan@gmail.com>
2015-03-17 23:43:59 +0800

    android: 屏蔽不可用的cctv频道

--- android/SailorCast/src/main/java/com/crixmod/sailorcast/model/SCLiveStreamType.java
+++ android/SailorCast/src/main/java/com/crixmod/sailorcast/model/SCLiveStreamType.java
@@ -8,17 +8,17 @@ public class SCLiveStreamType {
     private int typeId;
     public final static String TYPE_CAST_NAME = "轮播频道";  //轮播台
     public final static int TYPE_CAST = 0;  //轮播台
-    public final static String TYPE_CCTV_NAME = "央视频道";  //轮播台
-    public final static int TYPE_CCTV = 1;  //cctv
-    public final static int TYPE_PROVINCE_TV = 2;  //卫视频道
+    public final static int TYPE_PROVINCE_TV = 1;  //卫视频道
     public final static String TYPE_PROVINCE_TV_NAME = "卫视频道";  //卫视频道
+    public final static String TYPE_CCTV_NAME = "央视频道";  //轮播台
+    public final static int TYPE_CCTV = 2;  //cctv
 
     public SCLiveStreamType(int typeId) {
         this.typeId = typeId;
     }
 
     public static int getTypeCount() {
-        return 3;
+        return 2;
     }
 
     public int getTypeId() {
